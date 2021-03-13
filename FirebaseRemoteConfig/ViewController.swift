7c02f112f0cb3166707d1b37e8f1295aa6578976
German Santos <germans@unam.mx>
2021-03-13 00:51:17 -0600

    first commit

---
+++ FirebaseRemoteConfig/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseRemoteConfig
+//
+//  Created by Germán Santos Jaimes on 13/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
